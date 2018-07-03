629d02b194c061494fb65f847c3e895871b31167
BrianLy <thangln1003@gmail.com>
2018-07-03 14:57:57 +0700

    Update SelectionSort comment

--- SortingAlgorithms/SortingAlgorithms/SortingAlgorithms/Sorts/02_SelectionSort.cs
+++ SortingAlgorithms/SortingAlgorithms/SortingAlgorithms/Sorts/02_SelectionSort.cs
@@ -2,6 +2,13 @@
 {
     public static class SelectionSort
     {
+        /*
+         * Idea:
+         * Cho 1 giá trị min, so sánh min với từng phần tử trong mảng, nếu phần tử nhỏ hơn min, đổi trỗ giá trị
+         * 
+         * Complexity: O(N^2)
+         * 
+         */
         public static void Sort(int[] arr)
         {
             for (int i = 0; i < arr.Length - 1; i++)
